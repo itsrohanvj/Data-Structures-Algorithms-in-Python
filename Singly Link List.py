10a97a1717dfaccc8977b9bf0f89ed4015c2e191
Rohan Vijay <rohanvj17@live.com>
2020-06-24 01:21:52 +0530

    Only Insertion of Values
    Insertion of Values at the Beginning, At a given position, and at the end.

---
+++ Singly Link List.py
@@ -0,0 +1,98 @@
+    #Node of a Singly Linked List
+class Node:
+#constructor
+    def __init__ (self):
+            self.data = None
+            self.next = None
+            self.length=0
+            self.head=None
+#method for setting the data field of the node
+    def setData(self,data):
+            self.data = data
+#method for getting the data field of the node
+    def getData(self):
+            return self.data
+#method for setting the next field of the node
+    def setNext(self,next):
+                self.next = next
+#method for getting the next field of the node
+    def getNext(self):
+            return self.next
+#returns trne if the node points to another node
+    def hasNext(self):
+            return self.next != None
+
+    
+    def listLength(self):
+        current = self.head
+        
+        count=0
+        while  current!=None:
+            count=count+1
+            p=current.hasNext()
+            
+            print(current.getData())
+            current = current.getNext()
+            
+    def inscrtAtBeginning(self,data):
+        newNode = Node()
+        newNode.setData(data)
+        if self.length == 0:
+            self.head= newNode
+        else:
+            newNode.setNext(self.head)
+            self.head = newNode
+        self.length += 1
+
+    def insertAtEnd(self,data):
+        newNode = Node()
+        newNode.setData(data)
+        current= self.head
+        while current.getNext() != None:
+            current = current.getNext()
+        current.setNext(newNode)
+        self.length+=1
+
+    def insertAtPos(self,pos,data):
+        
+        if pos > self.length or pos < 0:
+            return None
+        else:
+            if pos == 0:
+                self.insertAtBeg(data)
+            else:
+                if pos ==self.length :
+                    self.insertAtEnd(data)
+                else:
+                    newNode = Node()
+                    newNode.setData(data)
+                    count= 0
+                    current= self.head
+                    while count< pos-1:
+                        count+=1
+                        current = current.getNext()
+                newNode.setNext(current.getNext())
+                current.setNext(newNode)
+                self.length += 1
+
+n=Node()
+n.inscrtAtBeginning(4)
+n.insertAtEnd(5)
+n.insertAtEnd(7)
+n.insertAtPos(1,99)
+
+n.listLength()
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
