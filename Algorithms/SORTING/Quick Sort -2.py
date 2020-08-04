767fe1ee348ec4416bde031c2a9bb791635b53e2
Rohan Vijay <rohanvj17@live.com>
2020-08-05 03:21:07 +0530

    quick sort

---
+++ Algorithms/SORTING/Quick Sort -2.py
@@ -0,0 +1,24 @@
+def quickSort(A,low,high):
+   if low >= high:
+    return
+   pivot = A[low]
+   i = low+1
+   j = high
+   while i <= j:
+       while i <= j and A[i] <= pivot:
+           i = i + 1
+       while A[j] >= pivot and j >= i:
+           j = j -1
+       if j < i:
+           break
+       A[i], A[j] = A[j], A[i]
+
+   A[low], A[j] = A[j], A[low]
+   partitionPoint = j
+
+   quickSort(A,low,partitionPoint-1)
+   quickSort(A,partitionPoint+1,high)
+
+A = [5, 1, 32, 54, 12, 6, 32, 2, 5, 19, 99, 9]
+quickSort(A,0,len(A)-1)
+print(A)
---
+++ Algorithms/SORTING/Quick Sort.py
@@ -0,0 +1,28 @@
+
+def QuickSort(A, low, high):
+    if low < high:
+        pivot = Partition(A, low, high)
+        QuickSort(A, low, pivot - 1)
+        QuickSort(A, pivot + 1, high)
+
+
+def Partition(A, low, high):
+    pivot = low
+    swap(A, pivot, high)
+    for i in range(low, high):
+        if A[i] <= A[high]:
+            swap(A, i, low)
+            low += 1
+
+    swap(A, low, high)
+    return low
+
+def swap(A, x, y):
+    temp = A[x]
+    A[x] = A[y]
+    A[y] = temp
+
+
+A = [534, 246, 933, 127, 277, 321, 454, 565, 220]
+QuickSort(A, 0, len(A) - 1)
+print(A)
