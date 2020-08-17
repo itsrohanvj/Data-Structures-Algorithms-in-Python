36861b959ea33c1fba7873074a4db660d1be6a00
Rohan Vijay <rohanvj17@live.com>
2020-08-18 05:08:08 +0530

    IMP BINARY TREE OPERATIONS

--- Data Structures/IMP-Binary Tree.py
+++ Data Structures/IMP-Binary Tree.py
@@ -461,3 +461,17 @@ def fillNextSiblingsWithLevelOrderTraversal(root):
 
 		if node.right is not None:
 			q.enQueue(node.right)
+
+#--------------------------------------------
+#TO COMPUTE MIRROR.
+def MirrorOfBinaryTree(root):
+    if (root != None):
+        MirrorOfBinaryTree(root.left)
+        MirrorOfBinaryTree(root.right)
+
+        # swap the pointers in this node
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+    return root
